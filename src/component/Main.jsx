a674e0fb222a6d2a06fa78eed0d739244ce306aa
Rambo <tounsi.csscool123@gmail.com>
2024-12-04 18:44:26 +0100

    simple modif

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -22,7 +22,7 @@ function Main() {
       
       <div className="about" id='about'>
         <ScrollAnimation customClass="left-animate">
-          <p><h2>A Little <span className='green'>About</span> Me:</h2> <br></br>My name is <span className='green'>Rami Tounsi</span> and i am a computer science student in the Higher Institute of Computer Science and Multimedia in Sfax. I am 21 years old and i am obsessed with IT and i love learning new technologies related to IT,also i love listening to music and i do DJing and playing video games in my freetime.<br/> <Button variant="success" className='download'>Download my CV</Button></p>
+          <p><h2>A Little <span className='green'>About</span> Me:</h2> <br></br>My name is <span className='green'>Rami Tounsi</span> and i am a computer science student in the Higher Institute of Computer Science and Multimedia in Sfax. I am 21 years old and i am obsessed with IT and i love learning new technologies related to IT,also i love listening to music and i do DJing and playing video games in my freetime.<br/></p>
         </ScrollAnimation>
         <ScrollAnimation customClass="right-animate">
         <div className='pic-cont'>
@@ -118,7 +118,7 @@ function Main() {
           </div>
           <div className='cont'>
             <FontAwesomeIcon icon={faPhone} />
-            <p>+216 99 999 999</p>
+            <p>+216 28 057 385</p>
           </div>
         </div>
         <div className='social'>        
